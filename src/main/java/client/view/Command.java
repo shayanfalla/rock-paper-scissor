88001f8c54bff3bbfb06ffbfdae554affca4938c
Shayan <shayanf@kth.se>
2018-01-06 18:46:53 +0100

    client work work

--- src/main/java/client/view/Command.java
+++ src/main/java/client/view/Command.java
@@ -1,12 +1,8 @@
 package client.view;
 
 public enum Command {   
-    CONNECT,
     PLAY,
-    LOGIN,
-    LOGOUT,
     HELP,
     ILLEGAL_COMMAND,
-    NO_COMMAND,
     QUIT
 }
--- src/main/java/client/view/GameInterpreter.java
+++ src/main/java/client/view/GameInterpreter.java
@@ -12,18 +12,12 @@ public class GameInterpreter implements Runnable {
     private Game game;
     private PlayerDTO player = null;
     private boolean loggedIn = false;
-    private NotificationHandler notif;
     private String username;
     private String move;
     private final ThreadSafeStdOut outMgr = new ThreadSafeStdOut();
 
     public void start(Game gm) {
         this.game = gm;
-        try {
-            this.notif = new NotificationHandler();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
         if (loggedIn) {
             return;
         }
@@ -36,20 +30,20 @@ public class GameInterpreter implements Runnable {
     public void run() {
         outMgr.println("~~Welcome to the Game~~");
         boolean playing = false;
-        while (true) {
+        while (true) {  
             try {
-                System.out.println();
+                outMgr.println("");
                 outMgr.println("Enter command: ('help' for list of commands)");
                 CmdLine cmdLine = new CmdLine(readNextLine());
 
                 switch (cmdLine.getCmd()) {
                     case HELP:
-                        System.out.println("~~The commands are~~");
+                        outMgr.println("~~The commands are~~");
                         for (Command command : Command.values()) {
-                            if (command == Command.ILLEGAL_COMMAND || command == Command.NO_COMMAND) {
+                            if (command == Command.ILLEGAL_COMMAND) {
                                 continue;
                             }
-                            System.out.println(command.toString().toLowerCase());
+                            outMgr.println(command.toString().toLowerCase());
                         }
                         break;
                     case PLAY:
@@ -59,33 +53,35 @@ public class GameInterpreter implements Runnable {
 
                         playing = true;
                         if (game.gameInSession()) {
-                            System.out.println("Game has already started. You will enter in the next round!");
+                            outMgr.println("Game has already started. You will enter in the next round!");
                             while (game.gameInSession()) {
-
                             }
                         }
                         outMgr.println("Enter a move or choose 'stop' to finish the game!");
                         game.playGame();
-
                         while (playing) {
-                            move = readNextLine().toUpperCase();
-                            switch (move) {
-                                case "SCISSOR":
-                                    game.sendMove("SCISSOR", username);
-                                    break;
-                                case "ROCK":
-                                    game.sendMove("ROCK", username);
-                                    break;
-                                case "PAPER":
-                                    game.sendMove("PAPER", username);
-                                    break;
-                                case "STOP":
-                                    game.leaveGame();
-                                    game.deletePlayer(username);
-                                    System.exit(0);
-                                default:
-                                    System.out.println("Invalid command! Try again!");
-                                    break;
+                            try {
+                                move = readNextLine().toUpperCase();
+                                switch (move) {
+                                    case "SCISSOR":
+                                        game.sendMove("SCISSOR", username);
+                                        break;
+                                    case "ROCK":
+                                        game.sendMove("ROCK", username);
+                                        break;
+                                    case "PAPER":
+                                        game.sendMove("PAPER", username);
+                                        break;
+                                    case "STOP":
+                                        game.leaveGame();
+                                        game.deletePlayer(username);
+                                        System.exit(0);
+                                    default:
+                                        outMgr.println("Invalid command! Try again!");
+                                        break;
+                                }
+                            } catch (Exception e) {
+                                outMgr.println("Ops, something happened! Be careful.");
                             }
                         }
                     case QUIT:
