41c6f728c8637d1026c429aa7b2a95acfedfa341
Shayan <shayanf@kth.se>
2018-01-06 19:12:41 +0100

    roundscore

--- src/main/java/server/model/GameLogic.java
+++ src/main/java/server/model/GameLogic.java
@@ -7,31 +7,32 @@ import java.util.logging.Logger;
 import server.controller.Controller;
 
 public class GameLogic {
+
     public void game(List<Player> players) throws RemoteException {
         int roundscore;
-        for(Player p1 : players) {
+        for (Player p1 : players) {
             String move1 = p1.getMove();
             roundscore = 0;
-            for(Player p2 : players) {
-                if(!p1.getUsername().equals(p2.getUsername())) {
+            for (Player p2 : players) {
+                if (!p1.getUsername().equals(p2.getUsername())) {
                     String move2 = p2.getMove();
-                    
-                    if(move1.equals("PAPER")) {
-                        p1.setScore(move2.equals("ROCK") ? 1 : 0);
+
+                    if (move1.equals("PAPER") && move2.equals("ROCK")) {
+                        p1.setScore(1);
                         roundscore++;
                     }
-                    if(move1.equals("ROCK")) {
-                        p1.setScore(move2.equals("SCISSOR") ? 1 : 0);
+                    if (move1.equals("ROCK") && move2.equals("SCISSOR")) {
+                        p1.setScore(1);
                         roundscore++;
                     }
-                    if(move1.equals("SCISSOR")) {
-                        p1.setScore(move2.equals("PAPER") ? 1 : 0);
+                    if (move1.equals("SCISSOR") && move2.equals("PAPER")) {
+                        p1.setScore(1);
                         roundscore++;
                     }
                 }
             }
             p1.getPlayerObj().recvMsg("Your score for this round was: " + roundscore);
         }
-            new Controller().updateInfo(players);
+        new Controller().updateInfo(players);
     }
 }
